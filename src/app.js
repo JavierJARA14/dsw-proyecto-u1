d184a2cb0f6957c34b99e8486205fdb8e7a35b9a
JavierJARA14 <jara.javier.jtv@gmail.com>
2025-10-25 21:20:22 -0700

    Estructura básica del proyecto

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ app.get("/", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
 
-app.use("/api/products", taskRoutes);
-app.use("/api/users", authRoutes);
+app.use("/api/products", /*taskRoutes*/);
+app.use("/api/users", /*authRoutes*/);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
